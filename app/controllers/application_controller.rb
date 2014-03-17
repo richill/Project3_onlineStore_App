ab62bad773ba713b1d61e8234a52e96087e3296a
Richill Tamakloe <richill.tamakloe@gmail.com>
2014-03-17 20:20:42 +0000

    partially created shopping cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,12 @@ class ApplicationController < ActionController::Base
 
 
   private
-    def current_cart Cart.find(session[:cart_id])
+  def current_cart 
+    Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound 
-      cart = Cart.create session[:cart_id] = cart.id
-      cart
-    end
+    cart = Cart.create 
+    session[:cart_id] = cart.id
+    cart
+  end
 end
 
