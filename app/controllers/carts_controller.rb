c3f246a3fd68622778bf387bb8fc2deaeeb31b69
Richill Tamakloe <richill.tamakloe@gmail.com>
2014-03-19 18:10:20 +0000

    updated CSS styling

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -17,7 +17,7 @@ class CartsController < ApplicationController
       @cart = Cart.find(params[:id]) 
     rescue ActiveRecord::RecordNotFound
       logger.error "Attempt to access invalid cart #{params[:id]}"
-      redirect_to store_url, notice: 'Invalid cart' 
+      redirect_to home_index_url, notice: 'Invalid cart' 
     else
       respond_to do |format|
       format.html # show.html.erb 
@@ -82,7 +82,7 @@ class CartsController < ApplicationController
     session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to store_url,
+      format.html { redirect_to home_index_url,
         notice: 'Your cart is currently empty' } 
       format.json { head :no_content }
     end 
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,7 @@
 class Cart < ActiveRecord::Base
   # attr_accessible :title, :body
   has_many :line_items, dependent: :destroy
+  belongs_to :user
 
   def add_product(product_id)
     current_item = line_items.find_by_product_id(product_id) 
