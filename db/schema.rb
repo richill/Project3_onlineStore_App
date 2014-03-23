69edce327498011656abe86ae6173e8d0da5f3d2
Richill Tamakloe <richill.tamakloe@gmail.com>
2014-03-23 23:24:02 +0000

    scaffold Admin | updated files index.html.erb - admin and application.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140323193855) do
+ActiveRecord::Schema.define(:version => 20140323224258) do
 
   create_table "accessories", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "admins", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
